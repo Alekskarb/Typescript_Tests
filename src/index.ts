f2285e67aea0e85087550b539ce2ff353f26f607
Alex Korbutovski <akorbutovski@elinext.com>
2023-08-17 16:38:21 +0300

    next test-2 TypeScript

--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,7 @@ function decorator(_: string) {
 class User {
     @decorator('Max')
     name: string
+
     constructor(name: string) {
         this.name = name;
     }
@@ -52,13 +53,78 @@ interface ILoan {
 }
 
 const totalPrice = (object: ILoan): number => {
-if (object.isInstallment) {
-    return (object.price / object.months) * ( 1 - object.discount / 100 );
-}
+    if (object.isInstallment) {
+        return (object.price / object.months) * (1 - object.discount / 100);
+    }
     return (object.price / object.months);
 };
 
-const price = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
+const price = totalPrice({price: 100000, discount: 25, isInstallment: true, months: 12});
 console.log(price);
 
+interface IPost {
+    id: string,
+    title: string,
+    body: string
+}
 
+const posts: IPost[] = [
+    {
+        id: '62e69d5a5458aac0ed320b35',
+        title: 'id labore ex et quam laborum',
+        body: 'laudantium enim quasi est quidem magnam voluptate ipsam eostempora quo necessitatibusdolor quam autem quasireiciendis et nam sapiente accusantium'
+    },
+    {
+        id: '62e69d5a5458aac0ed320b1c',
+        title: 'quo vero reiciendis velit similique earum',
+        body: 'est natus enim nihil est dolore omnis voluptatem numquamet omnis occaecati quod ullam at voluptatem error expedita pariaturnihil sint nostrum voluptatem reiciendis et'
+    },
+    {
+        id: '62e69d5a5458aac0ed320b32',
+        title: 'odio adipisci rerum aut animi',
+        body: 'quia molestiae reprehenderit quasi aspernaturaut expedita occaecati aliquam eveniet laudantiumomnis quibusdam delectus saepe quia accusamus maiores nam estcum et ducimus et vero voluptates excepturi deleniti ratione'
+    },
+    {
+        id: '62e69d5a5458aac0ed320b39',
+        title: 'alias odio sit',
+        body: 'non et atqueoccaecati deserunt quas accusantium unde odit nobis qui voluptatemquia voluptas consequuntur itaque doloret qui rerum deleniti ut occaecati'
+    },
+    {
+        id: '62e69d5a5458aac0ed320b53',
+        title: 'vero eaque aliquid doloribus et culpa',
+        body: 'harum non quasi et rationetempore iure ex voluptates in rationeharum architecto fugit inventore cupiditatevoluptates magni quo et'
+    },
+    {
+        id: '62e69d5a5458aac0ed320b19',
+        title: 'et fugit eligendi deleniti quidem qui sint nihil autem',
+        body: 'doloribus at sed quis culpa deserunt consectetur qui praesentiumaccusamus fugiat dictavoluptatem rerum ut voluptate autemvoluptatem repellendus aspernatur dolorem in'
+    },
+    {
+        id: '62e69d5a5458aac0ed320b25',
+        title: 'repellat consequatur praesentium vel minus molestias voluptatum',
+        body: 'maiores sed dolores similique labore et inventore etquasi temporibus esse sunt id eteos voluptatem aliquamratione corporis molestiae mollitia quia et magnam dolor'
+    }
+];
+
+interface INewPost {
+    byId: { [key: string]: IPost },
+    allIds: string[]
+}
+// function normalizeData<T, R>(a: T extends string, b: R): { byId: T } & { allIds: R } {
+function normalizeData(value: IPost[]): any {
+    let x = value.map(e => e.id);
+    // return value.forEach(post => {
+       return Object.assign( {byId: value} as Object, {allIds: x})
+    // });
+}
+console.log(normalizeData(posts));
+/**
+ * {
+ *    byId: {
+ *      62e69d5a5458aac0ed320b35: { id: '...', title: '...', body: '...' },
+ *      62e69d5a5458aac0ed320b1c: { id: '...', title: '...', body: '...' },
+ *      ...
+ *    },
+ *    allIds: ['62e69d5a5458aac0ed320b35', '62e69d5a5458aac0ed320b1c', ...]
+ * }
+ */
